5da075948a3a0f1108875ef49fbc7940b133427e
AntonyPDF <79217592+AntonyPDF@users.noreply.github.com>
2021-02-17 14:25:13 -0300

    Update Misc.py

--- DiscordBot/cogs/Misc.py
+++ DiscordBot/cogs/Misc.py
@@ -39,10 +39,6 @@ class Misc(commands.Cog):
     async def gay(self, ctx):
         await ctx.send('{} is gay'.format(ctx.message.author))
 
-    @commands.group(name='chad', help='Shows chad')
-    async def chad(self, ctx):
-        await ctx.send(file = discord.File('C:/Users/admin/Desktop/Images Bot/gigachad.jpg'))
-
     @commands.group(name='dream', help='Shows chads dream')
     async def dream(self, ctx):
         await ctx.send(file = discord.File('C:/Users/admin/Desktop/Images Bot/Dream.PNG'))
@@ -51,13 +47,5 @@ class Misc(commands.Cog):
     async def clear(self, ctx, a:int):
         await ctx.channel.purge(limit = a)
     
-    @commands.group(name='gostoso', help='Shows gostoso')
-    async def gostoso(self, ctx):
-        await ctx.send(file = discord.File('C:/Users/admin/Desktop/Images Bot/gostoso.PNG'))
-
-    @commands.group(name='gostosa', help='Shows gostosa')
-    async def gostosa(self, ctx):
-        await ctx.send(file = discord.File('C:/Users/admin/Desktop/Images Bot/gostosa.PNG'))
-
 def setup(client):
     client.add_cog(Misc(client))
