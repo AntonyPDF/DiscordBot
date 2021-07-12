4f9ca9224dd1480611d035fc8eb76914454980e9
AntonyPDF <79217592+AntonyPDF@users.noreply.github.com>
2021-07-12 09:53:56 -0300

    Update Misc.py

--- DiscordBot/cogs/Misc.py
+++ DiscordBot/cogs/Misc.py
@@ -19,8 +19,21 @@ import logging
 from urllib import request
 import codecs
 import aiohttp
+from itertools import permutations
 from bs4 import BeautifulSoup
 from random import randrange
+from googlesearch import search 
+
+def Permutations(n): 
+    a = permutations(n)
+    b = []
+    c = 'No strings with length longer than 5'
+    if len(n) > 5:
+        return c
+    else:
+        for i in a:
+            b.append(''.join(i))     
+    return b 
 
 class Misc(commands.Cog):
 
@@ -34,6 +47,14 @@ class Misc(commands.Cog):
     @commands.group(name='rng', help='Generates a random number in a given limit')
     async def rng(self, ctx, a:int):
         await ctx.send(randrange(a))
+    
+    @commands.group(name='gay', help='Gay')
+    async def gay(self, ctx):
+        await ctx.send('{} is gay'.format(ctx.message.author))
+
+    @commands.group(name='chad', help='Shows chad')
+    async def chad(self, ctx):
+        await ctx.send(file = discord.File('C:/Users/admin/Desktop/Images Bot/gigachad.jpg'))
 
     @commands.group(name='dream', help='Shows chads dream')
     async def dream(self, ctx):
@@ -41,7 +62,19 @@ class Misc(commands.Cog):
     
     @commands.group(name='clear', help='Clears a specified quantity of messages')
     async def clear(self, ctx, a:int):
-        await ctx.channel.purge(limit = a)
+        await ctx.channel.purge(limit = a+1)
     
+    @commands.group(name='perm', help='Permutates a given( string')
+    async def perm(self, ctx, a:str):
+        await ctx.send(Permutations(a))
+    
+    @commands.group(name='armor', help='Gives armor defense on LOL based on the armor value')
+    async def armor(self, ctx, a:int):
+        await ctx.send(a/(a+100))
+
+    @commands.group(name='google', help='Googles a given term or sentence')
+    async def armor(self, ctx, a:str):
+        await ctx.send(search(a)[0])    
+
 def setup(client):
     client.add_cog(Misc(client))
